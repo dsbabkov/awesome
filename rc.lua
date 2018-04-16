87a70dbabd55bcdbddc09ff5f50f0b8a55bb8282
Dmitriy <dsbabkov@gmail.com>
2018-04-16 09:40:21 +0300

    Добавлен апплет nm

--- rc.lua
+++ rc.lua
@@ -558,3 +558,6 @@ end)
 client.connect_signal("focus", function(c) c.border_color = beautiful.border_focus end)
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 -- }}}
+
+awful.util.spawn_with_shell("pgrep -u $USER -x nm-applet > /dev/null || (nm-applet &)")
+
